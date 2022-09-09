e865dcf49ac01cf2111d75d9590358695af04d4c
Paul Wainaina <faowayne@gmail.com>
2022-09-08 19:41:11 -0700

    prints the size of various types

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,8 @@
  * main - print the string in the put function
  *
  * Description: using the main function
- * This program prints "programming is like building a multilingual puzzle
- * return: 0
+ * This program prints "programming is like building a multilingual puzzle"
+ * return: returns zero 
  */
 int main(void)
 {
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the function
+ *
+ * return: It will return zero when successful
+ */
+int main(void)
+{
+	printf("Size of an int %zu byte(s)", sizeof(int));
+	printf("size of a char %zu byte(s)", sizeof(char));
+	return (0);
+}
+
