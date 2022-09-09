0aa6203b3b3bc8dce73a8dc850f955bc4fcec1eb
Paul Wainaina <faowayne@gmail.com>
2022-09-08 18:19:46 -0700

    A C program that prints

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point
+ *
+ * puts: prints sent as standard output
+ *
+ * Return: returns zero
+ */
+
+int main(void)
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+
+}
