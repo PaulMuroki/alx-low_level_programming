89b7761744600573d548b1e8c49d79d78943da23
Paul Wainaina <faowayne@gmail.com>
2022-09-12 13:20:08 -0700

    Print in c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the put function
+ * main - Starting point
  *
- * Description: using the main function
- * This program prints "programming is like building a multilingual puzzle"
- * return: returns zero 
+ * printf: prints sent as standard output
+ *
+ * Return: returns zero
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+
 }
