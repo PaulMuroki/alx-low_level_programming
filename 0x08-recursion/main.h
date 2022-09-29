03eaceae6e5427c5f6f378528798611b61335fb3
Paul Wainaina <faowayne@gmail.com>
2022-09-29 11:15:45 -0700

    natural square root of a number

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,7 +6,6 @@ int factorial(int n);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int _pow_recursion(int x, int y);
-int _sqrt_recursion(int n);
-
+int power_operation(int n, int c);
 
 #endif
