f749407df312ac047e1435afb29158b696e83dbc
Paul Wainaina <faowayne@gmail.com>
2022-10-15 16:06:03 -0700

    Function pointers C

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,15 @@
+#include <stdlib.h>
 #include "function_pointers.h"
+
 /**
  * print_name - Function that prints a name
  * @name: sring to be printed
  * @f: function pointer
- * Return: Returns nothing
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if ((name != NULL) && (f != NULL))
-		f(name);
+	if (!name || !f)
+		return;
+	f(name);
 }
